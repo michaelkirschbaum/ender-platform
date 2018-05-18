fda20bb4ce6f3ea9aadbad2170b4e438733c5d88
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-05-17 23:05:48 -0700

    create root endpoint

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 var express = require('express')
 var app = express()
+
+const PORT = 8080
+
+app.get('/', (req, res) => {
+  res.send('Ender')
+})
+
+app.listen(PORT, '0.0.0.0')
+console.log(listening on port {$PORT})
